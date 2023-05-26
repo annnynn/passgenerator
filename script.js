0cf942d368ffda86f3291635edb45c4d24e22187
annyn <ennibondarenko@gmail.com>
2023-05-26 15:56:05 +0400

    done js

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
 const lengthSlider = document.querySelector(".pass-length input");
 options = document.querySelectorAll(".option input");
 passwordInput = document.querySelector(".input-box input");
+copyIcon = document.querySelector(".input-box img");
+const parent = document.querySelector(".scales");
 passIndicator = document.querySelector(".pass-strength span");
-passStrengthColor = document.querySelectorAll(".pass-strength rectangle");
+
 generateBtn = document.querySelector(".generate-button");
 const characters = {
-    lowercase: "abcdefghijklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    symbols: "!$%&|[](){}:;.,*+-#@<>~"
+	lowercase: "abcdefghijklmnopqrstuvwxyz",
+	uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	numbers: "0123456789",
+	symbols: "!$%&|[](){}:;.,*+-#@<>~"
 }
 
 const generatePassword = () => {
-    let staticPassword = "";
-    randomPassword = "",
-    passLength = lengthSlider.value;
-    options.forEach(option => {
-        if(option.checked){
-            staticPassword = staticPassword + characters[option.id];
-        }
-    })
+	let staticPassword = "";
+	randomPassword = "",
+		passLength = lengthSlider.value;
+	options.forEach(option => {
+		if (option.checked) {
+			staticPassword = staticPassword + characters[option.id];
+		}
+	})
 
-    for (let i = 0; i < passLength; i++) {
-       randomPassword = randomPassword + staticPassword[Math.floor(Math.random() * staticPassword.length)];
-    }
-   passwordInput.value = randomPassword;
+	for (let i = 0; i < passLength; i++) {
+		randomPassword = randomPassword + staticPassword[Math.floor(Math.random() * staticPassword.length)];
+	}
+	passwordInput.value = randomPassword;
 }
 
-updatePassIndicator = () => {
-    passIndicator.id = lengthSlider.value <= 8 ? "WEAK" : lengthSlider.value <= 10 ? "MEDIUM" : "STRONG";
-    document.querySelector(".pass-strength span").innerText = passIndicator.id;
+const copyPassword = async () => {
+	try {
+		await navigator.clipboard.writeText(passwordInput.value);
+		copyIcon.classList.add("green");
+		setTimeout(() => {
+			copyIcon.classList.remove("green");
+		}, "1000");
+	} catch (err) {
+		console.log("failed to copy: ", err);
+	}
 }
 
-const updateSlider = () => {
-    document.querySelector(".pass-length span").innerText = lengthSlider.value;
-    generatePassword();
-    updatePassIndicator();
+const removeStyles = () => {
+	Array.from(parent.children).forEach(child => {
+		child.classList.remove("tooWeak", "weak", "medium", "strong");
+	})
 }
+
+const updatePassIndicator = () => {
+	removeStyles();
+	if (lengthSlider.value <= 4) {
+		passIndicator.textContent = "TOO WEAK!";
+		parent.children[1].classList.add("tooWeak");
+	} else if (lengthSlider.value <= 8) {
+		passIndicator.textContent = "WEAK";
+		parent.children[1].classList.add("weak");
+		parent.children[2].classList.add("weak");
+	} else if (lengthSlider.value <= 12) {
+		passIndicator.textContent = "MEDIUM";
+		parent.children[1].classList.add("medium");
+		parent.children[2].classList.add("medium");
+		parent.children[3].classList.add("medium");
+	} else {
+		passIndicator.textContent = "STRONG";
+		parent.children[1].classList.add("strong");
+		parent.children[2].classList.add("strong");
+		parent.children[3].classList.add("strong");
+		parent.children[4].classList.add("strong");
+	}
+
+}
+const scalesColor = () => {
+	parent.children.forEach(child => {
+		if (lengthSlider.value < 4) {
+			child.classList.add("week");
+		}
+	})
+}
+
+function updateSlider() {
+	document.querySelector(".pass-length span").innerText = lengthSlider.value;
+	updatePassIndicator();
+}
+
 updateSlider();
 
+copyIcon.addEventListener("click", copyPassword);
 lengthSlider.addEventListener("input", updateSlider);
-generateBtn.addEventListener("click",generatePassword);
+generateBtn.addEventListener("click", generatePassword);
